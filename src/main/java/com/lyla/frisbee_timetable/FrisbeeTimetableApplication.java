c6b9a1b02bef6e46fbb0f9fbc87e8eeb6a1bce7f
Lyla <lyla.li.ldl@gmail.com>
2026-01-05 16:40:10 +0000

    make tests use Postgres via test profile

---
+++ src/main/java/com/lyla/frisbee_timetable/FrisbeeTimetableApplication.java
@@ -0,0 +1,13 @@
+package com.lyla.frisbee_timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrisbeeTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrisbeeTimetableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyla/frisbee_timetable/FrisbeeTimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyla.frisbee_timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class FrisbeeTimetableApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
